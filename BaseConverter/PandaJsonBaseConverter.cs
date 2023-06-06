a999a3f0de1b2b46752c5896f2d4cb443aca3d1e
DefGh <defghq@gmail.com>
2023-06-06 15:50:24 +0400

    123

--- BaseConverter/PandaJsonBaseConverter.cs
+++ BaseConverter/PandaJsonBaseConverter.cs
@@ -1,9 +1,11 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BaseConverter;
 
-public class PandaJsonBaseConverter: JsonConverter<long>
+public class PandaJsonBaseConverter: JsonConverter<long>, IParameterModelConvention
 {
     public override long Read(
         ref Utf8JsonReader reader,
@@ -14,5 +16,36 @@ public class PandaJsonBaseConverter: JsonConverter<long>
     public override void Write(Utf8JsonWriter writer, long value, JsonSerializerOptions options)
     {
         writer.WriteStringValue(PandaBaseConverter.Base10ToBase36(value));
+    }
+
+    public void Apply(ParameterModel parameter)
+    {
+        if (parameter.ParameterType != typeof(long)) throw new Exception("Parameter type must be long");
+        parameter.BindingInfo ??= new BindingInfo();
+        parameter.BindingInfo.BinderType = typeof(StringToLongModelBinder);
+    }
+    
+    private class StringToLongModelBinder: IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+
+            var valueAsString = valueProviderResult.FirstValue;
+            if (string.IsNullOrEmpty(valueAsString))
+            {
+                return Task.CompletedTask;
+            }
+
+            var result = PandaBaseConverter.Base36ToBase10(valueAsString);
+            bindingContext.Result = ModelBindingResult.Success(result);
+            return Task.CompletedTask;
+        }
     }
 }
---
+++ Test/Controller.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
+using BaseConverter;
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Test;
+
+[Controller]
+public class Controller
+{
+    [HttpGet("Test/{testEnum}")]
+    public string Test([EndpointStringToLongConverter]  long testEnum)
+    {
+        return testEnum.ToString();
+    }
+    
+
+
+    
+    
+
+}
+
+class SomeModel
+{
+    [JsonConverter(typeof(PandaJsonBaseConverter))]
+    public long Id { get; set; } 
+}
+
+public class EndpointStringToLongConverter: Attribute, IParameterModelConvention
+{
+    public void Apply(ParameterModel parameter)
+    {
+        if (parameter.ParameterType == typeof(long))
+        {
+            parameter.BindingInfo ??= new BindingInfo();
+            parameter.BindingInfo.BinderType = typeof(StringToLongModelBinder);
+        }
+        
+    }
+}
+
+public class StringToLongModelBinder: IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+        if (valueProviderResult == ValueProviderResult.None)
+        {
+            return Task.CompletedTask;
+        }
+
+        bindingContext.ModelState.SetModelValue(bindingContext.ModelName, valueProviderResult);
+
+        var valueAsString = valueProviderResult.FirstValue;
+        if (string.IsNullOrEmpty(valueAsString))
+        {
+            return Task.CompletedTask;
+        }
+
+        var result = PandaBaseConverter.Base36ToBase10(valueAsString);
+        bindingContext.Result = ModelBindingResult.Success(result);
+        return Task.CompletedTask;
+    }
+}
+
